bb4be758bf4660742f05af8c2d0e2b851b17646a
Xephyr826 <xephyr826@gmail.com>
2017-01-14 11:06:53 -0800

    Add validation to register.php
    Started validation. Added validation functions and tested displaying
    errors.

--- globitek/public/register.php
+++ globitek/public/register.php
@@ -8,19 +8,47 @@
   // Hint: private/functions.php can help
   if (is_post_request()) {
     // Confirm that POST values are present before accessing them.
+
     $first_name = isset($_POST['first_name']) ? $_POST['first_name'] : '';
     $last_name = isset($_POST['last_name']) ? $_POST['last_name'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $username = isset($_POST['username']) ? $_POST['username'] : '';
 
-    echo 'First name: '. htmlentities($first_name) . '<br />';
-    echo 'Last name: ' . htmlentities($last_name) . '<br />';
-    echo 'E -mail: ' . htmlentities($email) . '<br />'; 
-    echo 'Last name: ' . htmlentities($username) . '<br />';
-
-
     // Perform Validations
     // Hint: Write these in private/validation_functions.php
+    $errors = [];    
+    //validate first_name
+    if (is_blank($_POST['first_name'])) {
+      $errors[] = "First name cannot be blank.";
+    } elseif (!has_length($_POST['first_name'], ['min' => 2, 'max' => 30])) {
+      $errors[] = "First name must be between 2 and 30 characters.";
+    }
+    //validate last_name
+    if (is_blank($_POST['last_name'])) {
+      $errors[] = "Last name cannot be blank.";
+    } elseif (!has_length($_POST['last_name'], ['min' => 2, 'max' => 30])) {
+      $errors[] = "Last name must be between 2 and 30 characters.";
+    }
+    //validate email
+    if (is_blank($_POST['email'])) {
+      $errors[] = "E-mail cannot be blank.";
+    } elseif (!has_length($_POST['email'], ['min' => 2, 'max' => 255])) {
+      $errors[] = "E-mail must be between 2 and 30 characters.";
+    }    
+    //validate username
+    if (is_blank($_POST['username'])) {
+      $errors[] = "User Name cannot be blank.";
+    } elseif (!has_length($_POST['email'], ['min' => 2, 'max' => 255])) {
+      $errors[] = "User Name must be between 2 and 30 characters.";
+    } elseif (!has_valid_email_format($email)) {
+      //check for valid email address
+      $errors[] = "Please provide a valid e-mail address.";
+    }
+
+    //as a test, display errors here
+    echo display_errors($errors);
+
+    
   }
     // if there were no errors, submit data to database
 
